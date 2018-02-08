8d3bdd4c4013bba55fcd84de5ac08dc9a0f72df1
JulianCRA <jj.juliancr@gmail.com>
2018-02-08 22:30:36 +0700

    Added some DB connection support

---
+++ PurchaseOrders/DatabaseConnection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchaseOrders
+{
+    public static class DatabaseConnection
+    {
+        
+        private static SqlConnection dbconn;
+
+        public static String connError { get; private set; }
+
+        private static void setConn()
+        {
+            dbconn = new SqlConnection("user id=applogin;" +
+                                        "password=password;" +
+                                        "server=localhost;" +
+                                        //"Trusted_Connection=yes;" +
+                                        "MultipleActiveResultSets=True;" +
+                                        "database=TestDB; " +
+                                        "connection timeout=10");
+        }
+
+        public static SqlConnection getConn()
+        {
+            if (dbconn == null || dbconn.State == ConnectionState.Closed)
+            {
+                startDBConnection();
+            }
+            return dbconn;
+        }
+
+        public static Boolean startDBConnection()
+        {
+            setConn();
+            try
+            {
+                dbconn.Open();
+            }
+            catch (SqlException e)
+            {
+                connError = e.Message;
+                return false;
+            }
+            return true;
+        }
+
+        public static String getNextAvailableID(String tname)
+        {
+            SqlCommand command = new SqlCommand("GetNextAvailableID", getConn());
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@tbname", tname);
+
+            SqlParameter returnParameter = command.Parameters.Add("RetVal", SqlDbType.Int);
+            returnParameter.Direction = ParameterDirection.ReturnValue;
+            command.ExecuteNonQuery();
+
+            int id = (int)returnParameter.Value;
+            return id.ToString().PadLeft(7, '0');
+        }
+        
+    }
+}
---
+++ PurchaseOrders/Suppliers/Supplier.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PurchaseOrders
+{
+    class Supplier : IDatabaseEntity
+    {
+        public String id { get; set; }
+
+        public String name { get; set; }
+
+        public String email { get; set; }
+
+        public Supplier(String i, String n, string e)
+        {
+            this.id = i;
+            this.name = n;
+            this.email = e;
+        }
+
+        public Boolean IsValid()
+        {
+            if (String.IsNullOrWhiteSpace(this.id)) return false;
+            if (String.IsNullOrWhiteSpace(this.name)) return false;
+            if (String.IsNullOrWhiteSpace(this.email)) return false;
+
+            Regex rgx = new Regex(@"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$");
+            if (!rgx.IsMatch(this.email)) return false;
+
+            rgx = new Regex(@"^[\d]{0,6}[\d]$");
+            if (!rgx.IsMatch(this.id)) return false;
+
+            rgx = new Regex(@"^(?=\S)( |.){0,29}\S$");
+            if (!rgx.IsMatch(this.name)) return false;
+
+            return true;
+        }
+    }
+}
