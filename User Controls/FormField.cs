ee922271c418f19208032f5d2fd264714dc70dc6
JulianCRA <jj.juliancr@gmail.com>
2018-02-08 19:48:36 +0700

    Created a user control for the form data validation.

---
+++ User Controls/FormField.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Text.RegularExpressions;
+
+namespace UserControls
+{
+    public partial class FormField : UserControl
+    {
+        private Regex rgx = new Regex(@".");
+
+        public Boolean isValid { get; set; }
+
+        public Boolean numericOnly { get; set; }
+
+        public String regEx
+        {
+            get { return rgx.ToString(); }
+            set { rgx = new Regex(value); }
+        }
+
+        public String text
+        {
+            get { return this.textBoxField.Text; }
+            set { this.textBoxField.Text = value; this.validateInput(); }
+        }
+
+        public String title
+        {
+            get { return this.labelTitle.Text; }
+            set { this.labelTitle.Text = value; }
+        }
+
+        // Property created so the max lenght can be manipulated to better control the user input
+
+        public Int32 textMaxLenght
+        {
+            get { return this.textBoxField.MaxLength; }
+            set { this.textBoxField.MaxLength = value; }
+        }
+
+        // Basically a binary switch that determines if the user should be able to manipukate the contents of the field
+
+        public Boolean assignManually
+        {
+            get { return this.textBoxField.Enabled; }
+            set
+            {
+                this.textBoxField.Enabled = value;
+                if (value) this.textBoxField.Focus();
+            }
+        }
+
+        public FormField()
+        {
+            InitializeComponent();
+            validateInput();
+            assignManually = true;
+        }
+
+        private void validateInput()
+        {
+            this.isValid = this.rgx.IsMatch(textBoxField.Text);
+            if (this.textBoxField.TextLength > 0)
+            {
+                if (this.isValid)
+                {
+                    this.reset();
+                }
+                else
+                {
+                    this.displayFeedback();
+                }
+            }
+            else
+            {
+                this.reset();
+            }
+        }
+
+        private void textBoxField_GotFocus(object sender, EventArgs e)
+        {
+            if(this.numericOnly)
+                this.textBoxField.KeyPress += new KeyPressEventHandler(this.textBoxField_KeyPress);
+            if (this.textBoxField.Text != String.Empty)
+            {
+                this.textBoxField.TextChanged += new System.EventHandler(this.textBoxField_TextChanged);
+            }
+        }
+
+        private void textBoxField_LostFocus(object sender, EventArgs e)
+        {
+            this.validateInput();
+            if (!this.isValid && this.textBoxField.Text != String.Empty)
+            {
+                this.displayFeedback();
+            }
+        }
+
+        private void textBoxField_TextChanged(object sender, EventArgs e)
+        {
+            this.validateInput();
+        }
+
+        private void displayFeedback()
+        {
+            this.labelError.Visible = true;
+            this.labelError.Text = "Erroneous input";
+        }
+
+        private void reset()
+        {
+            this.labelError.Visible = false;
+        }
+
+        // Method created as a way to let the user infer that the field should be filled with numbers only
+        // The criteria can be easily modified so it'll accept only certan set of characters
+
+        private void textBoxField_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+    }
+}
---
+++ User Controls/FormField.designer.cs
@@ -0,0 +1,116 @@
+﻿namespace UserControls
+{
+    partial class FormField
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxField = new System.Windows.Forms.TextBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelError = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel1.Controls.Add(this.textBoxField, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelTitle, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelError, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 46F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(320, 72);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBoxField
+            // 
+            this.textBoxField.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBoxField.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxField.Location = new System.Drawing.Point(13, 22);
+            this.textBoxField.Name = "textBoxField";
+            this.textBoxField.Size = new System.Drawing.Size(294, 30);
+            this.textBoxField.TabIndex = 0;
+            this.textBoxField.GotFocus += new System.EventHandler(this.textBoxField_GotFocus);
+            this.textBoxField.LostFocus += new System.EventHandler(this.textBoxField_LostFocus);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(10, 0);
+            this.labelTitle.Margin = new System.Windows.Forms.Padding(0);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(57, 19);
+            this.labelTitle.TabIndex = 1;
+            this.labelTitle.Text = "label1";
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Dock = System.Windows.Forms.DockStyle.Right;
+            this.labelError.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelError.ForeColor = System.Drawing.Color.Red;
+            this.labelError.Location = new System.Drawing.Point(264, 52);
+            this.labelError.Margin = new System.Windows.Forms.Padding(0);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(46, 20);
+            this.labelError.TabIndex = 2;
+            this.labelError.Text = "label1";
+            this.labelError.Visible = false;
+            // 
+            // FormField
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(320, 72);
+            this.MinimumSize = new System.Drawing.Size(120, 72);
+            this.Name = "FormField";
+            this.Size = new System.Drawing.Size(320, 72);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBoxField;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelError;
+    }
+}
